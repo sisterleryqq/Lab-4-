8eef8aa74f3239844f51c7a628c442a16545656f
Alena Rekunkova <alenarekunkova3017@gmail.com>
2023-10-19 16:30:57 +0300

    first commit

---
+++ Асинхронность в Node.js/app.js
@@ -0,0 +1,23 @@
+function display(data, callback) {
+    // с помощью случайного числа определяем ошибку
+    var randInt = Math.random() * (10 - 1) + 1;
+    var err =
+        randInt > 5
+            ? new Error(
+                  'Ошибка выполнения. randInt больше 5'
+              )
+            : null;
+
+    setTimeout(function () {
+        callback(err, data);
+    }, 0);
+}
+
+console.log('Начало работы программы');
+
+display('Обработка данных...', function (err, data) {
+    if (err) throw err;
+    console.log(data);
+});
+
+console.log('Завершение работы программы');
